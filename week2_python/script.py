8b20733010956cb5805499603c4c24000a8dcb99
RiyaAGeorge <rag2004@gmail.com>
2021-07-04 10:47:44 +0400

    First Commit

---
+++ week2_python/script.py
@@ -0,0 +1,17 @@
+coffeeOptions = ['Latte', 'Americano', 'Cappuccino', 'Espresso', 'Turkish']
+milkOptions = ['Dairy', 'Coconut', 'Almond', 'Hazeulnut','Oat','Soy']
+sizes = ['Tall','Grande','Venti']
+theOrder = []
+print("-----Welcome-----")
+print("MENU \n'Latte' \n 'Americano' \n 'Cappuccino' \n 'Espresso' \n 'Turkish'")
+print("Milk Optiions \n Dairy \n Coconut \n Almond \n Hazelnut \n Oat \n Soy")
+choice_coffee = input("Enter your preffered coffee type: ")
+choice_milk = input("Enter your milk choice: ")
+choice_size = input("Enter the prefered size: ")
+for choice_coffee in coffeeOptions:
+    print("Here's your order")
+    print(theOrder.insert[choice_milk])
+    print(theOrder.insert[choice_size])
+    print(theOrder)
+else:
+    print("Not applicable")
